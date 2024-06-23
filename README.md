4c49086a83b9e686256c1ab20fbf8c2fa391c77d
ddl <ddl196526@163.com>
2024-06-23 21:30:00 +0800

    feat: add deploy script

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 `git submodule update --init --recursive`
 ` git submodule update --force --recursive --remote`
 
+
+**deploy**
+
+forge script /script/AVS.s.sol:DeployAVS --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast -vvvv
---
+++ script/AVS.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.12;
+
+import {Script, console} from "forge-std/Script.sol";
+import {AVS} from "../src/AVS.sol";
+import {IDelegationManager} from "@eigenlayer/contracts/interfaces/IDelegationManager.sol";
+import {IAVSDirectory} from "@eigenlayer/contracts/interfaces/IAVSDirectory.sol";
+
+contract DeployAVS is Script {
+    function run() external {
+        // https://github.com/Layr-Labs/eigenlayer-contracts holesky 
+        address delegationManagerAddress = vm.envAddress("DELEGATION_MANAGER_ADDRESS");
+        address avsDirectoryAddress = vm.envAddress("AVS_DIRECTORY_ADDRESS");
+
+        vm.startBroadcast();
+
+        AVS avs = new AVS(IDelegationManager(delegationManagerAddress), IAVSDirectory(avsDirectoryAddress));
+
+        vm.stopBroadcast();
+
+        console.log("AVS deployed to:", address(avs));
+    }
+}
