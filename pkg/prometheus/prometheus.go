8ac7d08bd6c1613f34dd266da075b06ecfc7b9b4
ddl <ddl196526@163.com>
2024-08-05 12:17:08 +0800

    feat: split metrics

--- pkg/prometheus/prometheus.go
+++ pkg/prometheus/prometheus.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 func init() {
 	prometheus.MustRegister(AvsInfo)
+	prometheus.MustRegister(MemoryTotal)
+	prometheus.MustRegister(JobManagerStatus)
 
 }
 
@@ -19,16 +22,24 @@ var (
 	AvsInfo = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "avs_info",
-			Help: "AVS Info include: ip, hex_address, worker_memory_total_bytes, cpu model...",
+			Help: "AVS Info include: ip, hex_address, cpu model...",
 		},
-		[]string{"ip", "hex_address", "model", "worker_memory_total_bytes", "job_manager_status", "job"},
+		[]string{"ip", "hex_address", "model", "job"},
 	)
-
-	MemoryTotal = prometheus.NewGauge(
+	MemoryTotal = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "worker_memory_total_bytes",
 			Help: "Total memory of the worker in bytes",
 		},
+		[]string{"hex_address"},
+	)
+
+	JobManagerStatus = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "avs_job_manager_status",
+			Help: "Status of the AVS job manager (0 for inactive, 1 for active)",
+		},
+		[]string{"hex_address"},
 	)
 )
 
@@ -38,22 +49,29 @@ func UpdateHostMetrics(avsAddr string) {
 	cpuModel := GetCPUModel()
 	memTotal := GetTotalMemory()
 	flinkJobManagerStatus := GetFlinkJobManagerStatus()
-	AvsInfo.WithLabelValues(ip, avsAddr, cpuModel, fmt.Sprintf("%.2f", memTotal), flinkJobManagerStatus, JOB_NAME).Set(1)
+	AvsInfo.WithLabelValues(ip, avsAddr, cpuModel, JOB_NAME).Set(1)
+	MemoryTotal.WithLabelValues(avsAddr).Set(memTotal)
+	JobManagerStatus.WithLabelValues(avsAddr).Set(flinkJobManagerStatus)
 }
 
-func GetFlinkJobManagerStatus() string {
-	resp, err := http.Get("http://flink-jobmanager:8081/config")
+func GetFlinkJobManagerStatus() float64 {
+
+	flinkHostName := os.Getenv("FLINK_CONNECT_ADDRESS")
+	flinkHostPort := os.Getenv("FLINK_JOBMANAGER_PORT")
+	resp, err := http.Get((fmt.Sprintf("http://%s:%s/config", flinkHostName, flinkHostPort)))
+
 	if err != nil {
 
 		slog.Error("failed to get flink job manager status", "error", err)
-		return "0"
+		return 0
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
 		slog.Error("failed to get flink job manager status", "status_code", resp.StatusCode)
-		return "0"
+		return 0
 
 	}
-	return "1"
+
+	return 1
 }
