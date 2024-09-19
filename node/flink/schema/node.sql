dbb0ea8af58a91cfd2558fc99bc9a14b2f7d05d4
snake1361222 <snake13612227008@gmail.com>
2024-09-19 10:54:43 +0800

    <ADD> Generate proof of streaming task

---
+++ node/flink/schema/node.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE manuscript_node;
+
+\c manuscript_node;
+
+CREATE TABLE IF NOT EXISTS pow_results (
+    chain VARCHAR(255),
+    block_number BIGINT,
+    block_hash VARCHAR(255),
+    pow_result VARCHAR(255),
+    insert_at TIMESTAMP(3),
+    difficulty SMALLINT,
+    PRIMARY KEY (chain, block_number)
+);
+CREATE INDEX idx_pow_results_chain_block_number ON pow_results (chain, block_number);
